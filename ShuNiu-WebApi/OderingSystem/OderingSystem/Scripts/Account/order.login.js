1982653cff3751c3e87caa775717a80f571083b8
myangyy <yangming_i@126.com>
2018-03-29 13:31:30 +0800

    修改代码

--- ShuNiu-WebApi/OderingSystem/OderingSystem/Scripts/Account/order.login.js
+++ ShuNiu-WebApi/OderingSystem/OderingSystem/Scripts/Account/order.login.js
@@ -1,19 +1,85 @@
 ﻿var JSDEMO = function () {
+    var _v = {
+        account: {
+            Id: "",
+            RoleId: "",
+            Name: "",
+            ChineseName: "",
+            PassWord: "",
+        }
+
+    }
 	var _e = {
 		register: function () {
 			window.location.href = "/Page/Register.html";
-		}
+		},
+		login: function () {
+		    _v.user.Name = $("#login-username").val();
+		    _v.user.PassWord = $("#Password").val();
+		    _v.user.UserType = 1;
+		    if ($("#login-username").val() == "") {
+		        alert("用户名不能为空!");
+		        return false;
+		    }
+		    if ($("#Password").val() == "") {
+		        alert("密码不能为空!");
+		        return false;
+		    }
+		    $.ajax({
+		        type: "post",
+		        url: "odata/LoginAccount/AccountService.QueryAccount",
+		        //url: "odata/People",
+		        async: false,
+		        contentType: 'application/json',
+		        data: JSON.stringify({ "account": _v.account }),
+		        success: function (result) {
+		            if (result) {
+		                alert("删除成功");
+		                $('#list').empty();
+		                loadData();
+		            }
+		        },
+		        error: function (XMLHttpRequest, textStatus, errorThrown) {//请求失败处理函数
+		            console.log("请求失败，无法获取分组数据");
+		        }
+		    });
+		},
+
 	}
 	var _f = {
 		initClick: function () {
 			$("#register").click(function () {
 				_e.register();
 			});
-		}
+			$("#login-button").click(function () {
+			    _e.login();
+			});
+		},
+		initEvent: function () {
+		    $("#login-username").keypress(function (e) {
+		        if (e.keyCode == 13) {
+		            if ($("#login-username").val() == "") {
+		                alert("用户名不能为空!");
+		                return false;
+		            }
+		            $("#login-button").trigger('click');
+		        }
+		    });
+		    $("#Password").keypress(function (e) {
+		        if (e.keyCode == 13) {
+		            if ($("#Password").val() == "") {
+		                alert("密码不能为空!");
+		                return false;
+		            }
+		            $("#login-button").trigger('click');
+		        }
+		    });
+		},
 	}
 	return {
 		Init: function () {
-			_f.initClick();
+		    _f.initClick();
+		    _.initEvent();
 		}
 	};
 }();
